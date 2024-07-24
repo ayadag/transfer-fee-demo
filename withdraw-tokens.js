ba192ae94ef8a33f2e4ca140f893d388c2291b75
96ayad <96ayad@gmail.com>
2024-07-24 09:34:01 +0300

    config

--- withdraw-tokens.js
+++ withdraw-tokens.js
@@ -1,11 +1,24 @@
-import { Connection, Keypair, clusterApiUrl } from '@solana/web3.js';
+import bs58 from 'bs58';
+import dotenv from 'dotenv';
+
 import {
-  TOKEN_2022_PROGRAM_ID,
   getTransferFeeAmount,
+  TOKEN_2022_PROGRAM_ID,
   unpackAccount,
   withdrawWithheldTokensFromAccounts,
 } from '@solana/spl-token';
-import dotenv from 'dotenv';
+import {
+  clusterApiUrl,
+  Connection,
+  Keypair,
+  PublicKey,
+} from '@solana/web3.js';
+
+import {
+  PAYER,
+  RECIPIENT_KEYPAIR,
+} from './config-copy';
+
 dotenv.config();
 
 if (!process.env.RECIPIENT_KEYPAIR || !process.env.MINT_KEYPAIR ) {
@@ -15,20 +28,26 @@ if (!process.env.RECIPIENT_KEYPAIR || !process.env.MINT_KEYPAIR ) {
 const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
 
 const payer = Keypair.fromSecretKey(
-  new Uint8Array(JSON.parse(process.env.PAYER))
+  // new Uint8Array(JSON.parse(process.env.PAYER))
+  new Uint8Array.from(PAYER)
 );
 
-const mint = Keypair.fromSecretKey(
-  new Uint8Array(JSON.parse(process.env.MINT_KEYPAIR))
-).publicKey;
+// const mint = Keypair.fromSecretKey(
+//   new Uint8Array(JSON.parse(process.env.MINT_KEYPAIR))
+// ).publicKey;
+const mint = new PublicKey("Enjp5SF3Ft1oRJ7PnEm5t4xCV1rR9b6f4cCFH6QghZQB") //spl-2022
 
 const withdrawWithheldAuthority = Keypair.fromSecretKey(
-  new Uint8Array(JSON.parse(process.env.WITHDRAW_WITHHELD_AUTHORITY))
+  // new Uint8Array(JSON.parse(process.env.WITHDRAW_WITHHELD_AUTHORITY))
+  new Uint8Array.from(PAYER)
 );
 
 const recipientKeypair = Keypair.fromSecretKey(
-  new Uint8Array(JSON.parse(process.env.RECIPIENT_KEYPAIR))
+  // new Uint8Array(JSON.parse(process.env.RECIPIENT_KEYPAIR))
+  new Uint8Array.from(bs58.decode(RECIPIENT_KEYPAIR))
 );
+
+// const recipientPublicKey = new PublicKey('CLdt94RjT9Mnxh2jUFCiyDMsjfY158GBwt6bHtrcVb5L')
 
 const balance = await connection.getBalance(payer.publicKey);
 if (balance < 10000000) { // 0.01 SOL
@@ -93,6 +112,7 @@ console.log(
   `https://solana.fm/tx/${withdrawTokensSig}?cluster=devnet-solana`
 );
 
+console.log('withdrawTokensSig: ', withdrawTokensSig)
 // Optionally - you can also withdraw withheld tokens from the mint itself
 // see ReadMe for the difference
 
